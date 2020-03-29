2210fd981ad03e73d3070b1c85955b4ca7b9679e
Victor Hugo <vhuugoc@gmail.com>
2020-03-29 13:35:41 -0300

    migration: Criação da tabela de vendas

---
+++ api/database/migrations/2020_03_29_162422_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('description')->nullable(false);
+            $table->string('source')->nullable(false);
+            $table->float('value', 8, 2)->nullable(false);
+            $table->date('date')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
