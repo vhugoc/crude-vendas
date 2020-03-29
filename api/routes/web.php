847cd1927f8b45ad6f4e653c042e30b6f73116a0
Victor Hugo <vhuugoc@gmail.com>
2020-03-29 14:20:44 -0300

    Rota: Rotas de vendas adicionadas

--- api/routes/web.php
+++ api/routes/web.php
@@ -20,4 +20,16 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('/register', 'UserController@register');
     $router->post('/signin', 'UserController@signin');
     $router->get('/profile', ['middleware' => App\Http\Middleware\AuthorizationMiddleware::class, 'uses' => 'UserController@show']);
+
+    /**
+     * Sales Routing
+     * 
+     */
+    $router->group(['prefix' => 'sale'], function () use ($router) {
+        $router->get('/', ['middleware' => App\Http\Middleware\AuthorizationMiddleware::class, 'uses' => 'SaleController@index']);
+        $router->get('/{id}', ['middleware' => App\Http\Middleware\AuthorizationMiddleware::class, 'uses' => 'SaleController@show']);
+        $router->post('/', ['middleware' => App\Http\Middleware\AuthorizationMiddleware::class, 'uses' => 'SaleController@add']);
+        $router->put('/{id}', ['middleware' => App\Http\Middleware\AuthorizationMiddleware::class, 'uses' => 'SaleController@update']);
+        $router->delete('/{id}', ['middleware' => App\Http\Middleware\AuthorizationMiddleware::class, 'uses' => 'SaleController@delete']);
+    });
 });
